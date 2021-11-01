619e022f55acd30af01e29dbcec1b66547bd5a8c
moacyrsalim <moacyrsalim@gmail.com>
2021-10-31 21:02:41 -0300

    1052

---
+++ 1052.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int MES;
+
+    cin >> MES;
+
+    if(MES == 1){
+        cout << "January\n";
+    }
+    else if(MES == 2){
+        cout << "February\n";
+    }
+    else if(MES == 3){
+        cout << "March\n";
+    }
+    else if(MES == 4){
+        cout << "April\n";
+    }
+    else if(MES == 5){
+        cout << "May\n";
+    }
+    else if(MES == 6){
+        cout << "June\n";
+    }
+    else if(MES == 7){
+        cout << "July\n";
+    }
+    else if(MES == 8){
+        cout << "August\n";
+    }
+    else if(MES == 9){
+        cout << "September\n";
+    }
+    else if(MES == 10){
+        cout << "October\n";
+    }
+    else if(MES == 11){
+        cout << "November\n";
+    }
+    else if(MES == 12){
+        cout << "December\n";
+    }
+
+
+    return 0;
+}
